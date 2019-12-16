c1e798463274c1565a4c66dba9fc4b090b96772d
Yichuan Wang <w369371@gmail.com>
2019-12-16 14:24:26 +1000

    apply edamam api and parse json to object

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState} from 'react';
+import Recipe from './Recipe'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+
+  const APP_ID = "55810db7"
+  const APP_KEY = "3b759bf9832b2e2f2e72d827ea4b26ff"
+
+  const [search, setSearch] = useState("")
+  const [recipes, setRecpies] = useState([])
+
+  useEffect(() =>{getRecipes()}, [])
+
+  const getRecipes = async () =>{
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`)
+    const data = await response.json()
+    setRecpies(data.hits)
+ }
+  
+  return(
+      <div className="App">
+        <form className="search-form">
+          <input className="search-bar" type="text"/>
+         <button className="search-button" type="submit">Search</button>
+        </form>
+       {recipes.map(recipe => (
+         <Recipe key={recipe.recipe.label} title={recipe.recipe.label} calories={recipe.recipe.calories} image={recipe.recipe.image}/>
+       ))}
+      </div>
+  )
 }
 
 export default App;
